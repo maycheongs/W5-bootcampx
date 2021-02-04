825966eb9e3cd535b451b471a5988af46c9293ba
May C <maycheongs@gmail.com>
2021-02-04 01:20:20 +0000

    sanitized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
-
-pool.query(`
+const queryString = `
 SELECT students.id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const values = [`%${process.argv[2]}%`,`${process.argv[3] || 5}`];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(student => {
     console.log(`${student.name} has an id of ${student.id} and was in the ${student.cohort} cohort.`)
